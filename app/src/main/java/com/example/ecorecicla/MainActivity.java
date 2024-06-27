83f70f50851aaf0d617cc166a3d49edf2b636554
seergie <sergioadrian.beltrancampos2@gmail.com>
2024-06-26 22:46:46 -0500

    Conectando con una base de datos sqllite

--- app/src/main/java/com/example/ecorecicla/MainActivity.java
+++ app/src/main/java/com/example/ecorecicla/MainActivity.java
@@ -1,24 +1,38 @@
 package com.example.ecorecicla;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.models.DatabaseManager;
 
 public class MainActivity extends AppCompatActivity {
+    private DatabaseManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        dbManager = new DatabaseManager(this);
+
+        // Insertar usuario
+        dbManager.insertUser("Diana Carolina", "Mesa Niño", "52922518", "dianacmesa@gmail.com", "525317", "123458");
+
+        // Obtener usuarios
+        Cursor cursor = dbManager.getUsers();
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String email = cursor.getString(2);
+
+                // Mostrar datos en un TextView
+                TextView textView = findViewById(R.id.textView);
+                textView.append("ID: " + id + ", Name: " + name + ", Email: " + email + "\n");
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
     }
 }
