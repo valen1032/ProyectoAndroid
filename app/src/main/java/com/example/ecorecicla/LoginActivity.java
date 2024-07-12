f01a84201a3f9513856253633aae238500072678
seergie <sergioadrian.beltrancampos2@gmail.com>
2024-07-11 19:49:02 -0500

    subiendo la vista de restablecer contraseña

--- app/src/main/java/com/example/ecorecicla/LoginActivity.java
+++ app/src/main/java/com/example/ecorecicla/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,6 +27,29 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Iniciar ActivityLogin al hacer clic en registerButton
                 Intent intent = new Intent(LoginActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Encontrar el botón irRegisterButton en activity_login.xml
+        Button irRegisterButton = findViewById(R.id.irRegisterButton);
+
+        // Configurar OnClickListener para el botón
+        irRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar MainActivity al hacer clic en irRegisterButton
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView forgotPasswordLink = findViewById(R.id.forgotPasswordLink);
+        forgotPasswordLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ir a ForgotPasswordActivity
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
                 startActivity(intent);
             }
         });
